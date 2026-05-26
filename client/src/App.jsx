164e2f502ead63c65749cbd454889c13f3f9c122
Athul Mathew George <athulmathewgeorge@mgu.ac.in>
2026-05-26 08:46:57 +0530

    fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,6 @@ import RssReaderPage from './components/RssReaderPage';
 import LoginScreen from './components/LoginScreen';
 import FilesPage from './components/FilesPage';
 import { GoogleOAuthProvider } from '@react-oauth/google';
-import { config } from './config/environment';
 import './App.css';
 
 const AuthenticatedApp = () => {
@@ -65,7 +64,7 @@ const AuthenticatedApp = () => {
 };
 
 function App() {
-  const clientId = config.googleClientId;
+  const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID || 'dummy-client-id.apps.googleusercontent.com';
   return (
     <GoogleOAuthProvider clientId={clientId}>
       <AuthProvider>
